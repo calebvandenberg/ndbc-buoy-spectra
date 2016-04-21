16dc67faa2bdc5348e72bc2d62f15fc824383eb5
Alex Mattingley <alexmattingely@gmail.com>
2016-04-21 12:06:13 -0700

    ok this is a start to modularizing the flag setting proccess

--- routes/buoy.js
+++ routes/buoy.js
@@ -1,54 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var exec = require('child_process').exec;
+var set_flags = require('./set_flags');
 
 
-
-
-var pyArgs = {
-  // make arguments that take no parameters (ie, --json) true or false
-  "buoy": '46232',
-  "datasource": 'http',
-  "json": true,
-  "datatype": "spectra",
-  "units": 'ft'
-};
-//example
-pyArgs.datatype = '9band';
-
-function flagGen(args) {
-  var flags = '';
-  for (var a in args) {
-    if (args.hasOwnProperty(a)) {
-      if (typeof(pyArgs[a]) == 'string'){
-        flags += " --" + a + ' ' + pyArgs[a];
-      }
-      else {
-        if (pyArgs[a] == true)
-          flags += ' --' + a;
-      }
-    }
-  }
-  return flags;
-}
-
-var pyPath = './';
-var buoyData = ''
-var execstr = 'python ' + path.join(pyPath, 'ndbc.py') + flagGen(pyArgs);
-var child = exec(execstr, function(error, stdout, stderr) {
-  if (error) {
-    console.log(stderr)
-  }
-  else {
-    buoyData= JSON.parse(stdout);
-    console.log(buoyData);
-  }
-});
 
 /* GET buoy page. */
 router.get('/', function(req, res, next) {
   var buoy_id = req.query.buoy_id;
+  set_flags.run_script();
 	res.send(buoy_id);
 });
 
---
+++ routes/set_flags.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var exec = require('child_process').exec;
+
+var pyArgs = {
+  // make arguments that take no parameters (ie, --json) true or false
+  "buoy": '46232',
+  "datasource": 'http',
+  "json": true,
+  "datatype": "spectra",
+  "units": 'ft'
+};
+
+pyArgs.datatype = '9band';
+
+function flagGen(args) {
+  var flags = '';
+  for (var a in args) {
+    if (args.hasOwnProperty(a)) {
+      if (typeof(pyArgs[a]) == 'string'){
+        flags += " --" + a + ' ' + pyArgs[a];
+      }
+      else {
+        if (pyArgs[a] == true)
+          flags += ' --' + a;
+      }
+    }
+  }
+  return flags;
+}
+
+function run_script(){
+  var buoyData = ''
+  var execstr = 'python ' + path.join('./', 'ndbc.py') + flagGen(pyArgs);
+  var child = exec(execstr, function(error, stdout, stderr) {
+    if (error) {
+      console.log(stderr)
+    }
+    else {
+      buoyData= JSON.parse(stdout);
+      console.log(buoyData);
+    }
+  });
+}
+
+module.exports.run_script = run_script;
